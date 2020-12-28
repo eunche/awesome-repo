1f900e84a4cb6d795567d638c37f746ca25dca5e
eunchae619 <eunchae619@naver.com>
2020-12-28 13:28:42 +0900

    RoomsView,RoomView Viewset으로 변환(list,retrieve,create일때만 permission작업 완료

--- rooms/views.py
+++ rooms/views.py
@@ -1,84 +1,25 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import permissions
 from .models import Room
 from .serializers import RoomSerializer
 
 
-class OwnPagination(PageNumberPagination):
-    page_size = 20
+class RoomViewSet(ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
-
-class RoomsView(APIView):
-    def get(self, request):
-        paginator = OwnPagination()
-        rooms = Room.objects.all()
-        # request를 주는 이유는 ?page=2 이런걸 찾아내게 하려고
-        results = paginator.paginate_queryset(rooms, request)
-        serializer = RoomSerializer(results, many=True, context={"request": request})
-        return paginator.get_paginated_response(serializer.data)
-
-    def post(self, request):
-        # 로그인 된지 검사
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer = RoomSerializer(data=request.data)
-        # RoomSerializer 필드에 맞게 값이 들어왔는지 유효성 검사
-        if serializer.is_valid():
-            # 로그인된 user값을 create메소드로 넘겨서 방객체 생성하고 room변수에 저장
-            room = serializer.save(user=request.user)
-            # 새로 생성된 방을 RoomSerializer(디테일뷰같은)로 room_serializer에 저장
-            room_serializer = RoomSerializer(room).data
-            # 새로만든 방의 API와 HTTP응답코드를 return
-            return Response(data=room_serializer, status=status.HTTP_200_OK)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RoomView(APIView):
-    def get_room(self, pk):
-        try:
-            room = Room.objects.get(pk=pk)
-            return room
-        except Room.DoesNotExist:
-            return None
-
-    def get(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            serializer = RoomSerializer(room).data
-            return Response(serializer)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def put(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            if room.user != request.user:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            serializer = RoomSerializer(room, data=request.data, partial=True)
-            if serializer.is_valid():
-                room = serializer.save()
-                return Response(RoomSerializer(room).data)
-            else:
-                return Response(
-                    data=serializer.errors, status=status.HTTP_400_BAD_REQUEST
-                )
-            return Response()
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            if room.user != request:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            room.delete()
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":
+            permission_classes = [permissions.AllowAny]
+        elif self.action == "create":
+            permission_classes = [permissions.IsAuthenticated]
+        # else:
+        ###### permission_classes = [IsOwner]
 
 
 @api_view(["GET"])
